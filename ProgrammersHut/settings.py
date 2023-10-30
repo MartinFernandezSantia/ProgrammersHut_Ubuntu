4399406779668cd3cf05e3c56a757c5dfb6719d1
Martin Fernandez Santia <martin-santia@hotmail.com.ar>
2023-10-30 05:22:43 -0300

    Finished fixing cropper

--- ProgrammersHut/settings.py
+++ ProgrammersHut/settings.py
@@ -21,31 +21,33 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sc3s)12e0#)+7&ryyo$$8i6r_m1$4+6$dch80ac-jqto6q&9l6'
+SECRET_KEY = "django-insecure-sc3s)12e0#)+7&ryyo$$8i6r_m1$4+6$dch80ac-jqto6q&9l6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = ["*",]
+ALLOWED_HOSTS = [
+    "*",
+]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-	"django.contrib.sites",
+    "django.contrib.sites",
     "registration",  # should be immediately above 'django.contrib.admin'
     # Django channels
-    #"daphne",
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # "daphne",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     "ckeditor",
-	# apps terceros
+    # apps terceros
     "captcha",
     # mis apps
     "homepage.apps.HomepageConfig",
@@ -60,16 +62,16 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'ProgrammersHut.urls'
+ROOT_URLCONF = "ProgrammersHut.urls"
 
 TEMPLATES = [
     {
@@ -83,13 +85,13 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 # My processors
-                #"amigos.views.autocomplete_ajax",
+                # "amigos.views.autocomplete_ajax",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'ProgrammersHut.wsgi.application'
+WSGI_APPLICATION = "ProgrammersHut.wsgi.application"
 """ASGI_APPLICATION = "ProgrammersHut.asgi.application"
 CHANNEL_LAYERS = {
     "default": {
@@ -103,33 +105,39 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = { 
- 	'default': { 
-		 'ENGINE': 'django.db.backends.mysql',
-		 'NAME': 'ProgrammersHut', 
-		 'USER': 'esmoquin', 
-		 'PASSWORD': 'Jumper789',
-		 'HOST': 'localhost',
-		 'PORT': '3306',
-	} 
-}
+# DATABASES = {
+#  	'default': {
+# 		 'ENGINE': 'django.db.backends.mysql',
+# 		 'NAME': 'ProgrammersHut',
+# 		 'USER': 'esmoquin',
+# 		 'PASSWORD': 'Jumper789',
+# 		 'HOST': 'localhost',
+# 		 'PORT': '3306',
+# 	}
+# }
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -137,9 +145,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -151,7 +159,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static_dev"),)
 MEDIA_URL = "/media/"
@@ -160,7 +168,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/"
 LOGIN_URL = "django.contrib.auth.views.login"
@@ -172,20 +180,49 @@ SITE_ID = 1
 CKEDITOR_JQUERY_URL = "//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"
 CKEDITOR_UPLOAD_PATH = "faq_uplods/"
 CKEDITOR_CONFIGS = {
-    'default': {
-        'toolbar': (
-            ['div', 'Source', '-', 'Save', 'NewPage', 'Preview', '-', 'Templates'],
-            ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Print', 'SpellChecker', 'Scayt'],
-            ['Undo', 'Redo', '-', 'Find', 'Replace', '-', 'SelectAll', 'RemoveFormat'],
-            ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
-            ['Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript'],
-            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote'],
-            ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
-            ['Link', 'Unlink', 'Anchor'],
-            ['Image', 'Update', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak'],
-            ['Styles', 'Format', 'Font', 'FontSize'],
-            ['TextColor', 'BGColor'],
-            ['Maximize', 'ShowBlocks', '-', 'About', 'pbckcode'],
+    "default": {
+        "toolbar": (
+            ["div", "Source", "-", "Save", "NewPage", "Preview", "-", "Templates"],
+            [
+                "Cut",
+                "Copy",
+                "Paste",
+                "PasteText",
+                "PasteFromWord",
+                "-",
+                "Print",
+                "SpellChecker",
+                "Scayt",
+            ],
+            ["Undo", "Redo", "-", "Find", "Replace", "-", "SelectAll", "RemoveFormat"],
+            [
+                "Form",
+                "Checkbox",
+                "Radio",
+                "TextField",
+                "Textarea",
+                "Select",
+                "Button",
+                "ImageButton",
+                "HiddenField",
+            ],
+            ["Bold", "Italic", "Underline", "Strike", "-", "Subscript", "Superscript"],
+            ["NumberedList", "BulletedList", "-", "Outdent", "Indent", "Blockquote"],
+            ["JustifyLeft", "JustifyCenter", "JustifyRight", "JustifyBlock"],
+            ["Link", "Unlink", "Anchor"],
+            [
+                "Image",
+                "Update",
+                "Flash",
+                "Table",
+                "HorizontalRule",
+                "Smiley",
+                "SpecialChar",
+                "PageBreak",
+            ],
+            ["Styles", "Format", "Font", "FontSize"],
+            ["TextColor", "BGColor"],
+            ["Maximize", "ShowBlocks", "-", "About", "pbckcode"],
         ),
     }
 }
@@ -222,4 +259,3 @@ MESSAGE_TAGS = {
     messages.ERROR: "alert-danger",
     messages.SUCCESS: "alert-success",
 }
-
--- login/views.py
+++ login/views.py
@@ -2,58 +2,66 @@ from django.shortcuts import render
 from login.models import DatosUsuario
 from django.http import HttpResponseRedirect
 from django.contrib import messages
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.core.files import File
+
 # from django.conf import settings
 import re
 import requests
 import os
+
 # Create your views here.
 
 from pathlib import Path
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 def generate_avatar(seed):
     """
     The function `generate_avatar` takes a seed as input and returns the content of an SVG image
     generated using the DiceBear API.
-    
+
     :param seed: The seed parameter is a value that is used to generate a unique avatar. It can be any
     string or number that you choose
     :return: The function `generate_avatar` returns the content of the response if the status code is
     200 (indicating a successful request), otherwise it returns None.
     """
-    url = f"https://avatars.dicebear.com/api/bottts/{seed}.svg"
+    url = f"https://api.dicebear.com/7.x/bottts/svg?seed={seed}"
     response = requests.get(url)
     if response.status_code == 200:
         return response.content
     else:
         return None
 
+
 def is_anonymous(func):
     """
     The function `is_anonymous` is a decorator that checks if the user is anonymous and redirects them
     to the homepage if they are not.
-    
+
     :param func: The `func` parameter is a function that will be wrapped by the `is_anonymous` decorator
     :return: The function `check` is being returned.
     """
+
     def check(*args, **kwargs):
         if not args[0].user.is_anonymous:
-            messages.error(args[0],"Debe cerrar sesión para poder acceder")
-            return HttpResponseRedirect("/") 
-        
+            messages.error(args[0], "Debe cerrar sesión para poder acceder")
+            return HttpResponseRedirect("/")
+
         return func(*args, **kwargs)
+
     return check
+
 
 @is_anonymous
 def login(request):
     """
     The `login` function is a view function in Django that handles user authentication and login.
-    
+
     :param request: The `request` parameter is an object that represents the HTTP request made by the
     user. It contains information about the request, such as the method used (GET or POST), the headers,
     the user's IP address, and any data sent with the request
@@ -69,8 +77,10 @@ def login(request):
 
         if user is not None:
             auth_login(request, user)
-            messages.success(request, f"¡Hola! ¡Qué bueno verte por aquí de nuevo {username_}!")
-            return HttpResponseRedirect('/')
+            messages.success(
+                request, f"¡Hola! ¡Qué bueno verte por aquí de nuevo {username_}!"
+            )
+            return HttpResponseRedirect("/")
 
         messages.error(request, "El usuario o contraseña ingresados son incorrectos")
         return HttpResponseRedirect(request.path_info)
@@ -84,7 +94,7 @@ def register(request):
     """
     The `register` function is used to handle user registration, validate user input, and create a new
     user account if the input is valid.
-    
+
     :param request: The `request` parameter is an object that represents the HTTP request made by the
     user. It contains information such as the method used (GET or POST), the data sent in the request,
     and other metadata
@@ -100,46 +110,55 @@ def register(request):
         password1 = request.POST.get("registerPassword1")
         password2 = request.POST.get("registerPassword2")
 
-        password_pattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$" # Needs upper, lower, number and at least 8
-        email_pattern = r"^\S+@\S+\.\S+$" # Needs to match email pattern
-        username_pattern = "^[a-zA-Z0-9_-]{4,20}$" # Needs to match alphanumeric, _ , and from 4 to 20 char
+        password_pattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$"  # Needs upper, lower, number and at least 8
+        email_pattern = r"^\S+@\S+\.\S+$"  # Needs to match email pattern
+        username_pattern = "^[a-zA-Z0-9_-]{4,20}$"  # Needs to match alphanumeric, _ , and from 4 to 20 char
 
-        if User.objects.filter(username=username_).exists() or not re.match(username_pattern, username_):
-            messages.error(request, f'"{username_}" ya esta en uso o no cumple los requisitos')
+        if User.objects.filter(username=username_).exists() or not re.match(
+            username_pattern, username_
+        ):
+            messages.error(
+                request, f'"{username_}" ya esta en uso o no cumple los requisitos'
+            )
             return HttpResponseRedirect(request.path_info)
-        if User.objects.filter(email = mail).exists() or not re.match(email_pattern, mail):
+        if User.objects.filter(email=mail).exists() or not re.match(
+            email_pattern, mail
+        ):
             messages.error(request, f'"{mail}" ya esta en uso o no es un mail')
             return HttpResponseRedirect(request.path_info)
         elif not re.match(password_pattern, password1):
-            messages.error(request, "La contraseña ingresada es muy debil o no cumple los requisitos")
+            messages.error(
+                request,
+                "La contraseña ingresada es muy debil o no cumple los requisitos",
+            )
             return HttpResponseRedirect(request.path_info)
         elif password1 != password2:
             messages.error(request, "Las contraseñas no coinciden")
             return HttpResponseRedirect(request.path_info)
-
-        
 
         avatar_ = generate_avatar(username_)
         file_path = os.path.join(BASE_DIR, "media", "avatar", f"{username_}.svg")
         if avatar_:
             with open(file_path, "wb") as f:
                 f.write(avatar_)
-        user_ = User.objects.create_user(username=username_, email=mail, password=password1)
+        user_ = User.objects.create_user(
+            username=username_, email=mail, password=password1
+        )
         user_data = DatosUsuario(user=user_, avatar=f"avatar/{username_}.svg").save()
 
-
-        messages.success(request,"Tu cuenta ha sido exitosamente creada!!")
-        return HttpResponseRedirect('/')
+        messages.success(request, "Tu cuenta ha sido exitosamente creada!!")
+        return HttpResponseRedirect("/")
 
     params = {}
     params["nombre_sitio"] = "Crear una cuenta"
     return render(request, "login/register.html", params)
 
+
 @login_required
 def logout_request(request):
     """
     The function logs out the user, displays a success message, and redirects them to the homepage.
-    
+
     :param request: The `request` parameter is an object that represents the current HTTP request. It
     contains information about the request, such as the user making the request, the requested URL, and
     any data sent with the request
@@ -147,5 +166,5 @@ def logout_request(request):
     """
     logout(request)
 
-    messages.success(request,"¡Hasta la próxima! Esperamos verte pronto.")
-    return HttpResponseRedirect('/')
+    messages.success(request, "¡Hasta la próxima! Esperamos verte pronto.")
+    return HttpResponseRedirect("/")
